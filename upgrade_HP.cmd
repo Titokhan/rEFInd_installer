7c644579c73d8310df0e82d61ac2e3c5b1cfacbb
Tito <kingshuk_slg_007@yahoo.com>
2018-03-02 08:30:34 +0530

    Create upgrade_HP.cmd

---
+++ upgrade_HP.cmd
@@ -0,0 +1,10 @@
+mountvol S: /S
+del S:\EFI\Microsoft\Boot\refind_x64.bak
+del S:\EFI\Microsoft\bootmgfw.efi
+xcopy /E refind S:\EFI\Microsoft\Boot\
+copy S:\EFI\Microsoft\Boot\bootmgfw.efi S:\EFI\Microsoft\bootmgfw.efi
+del S:\EFI\Microsoft\Boot\bootmgfw.efi
+copy S:\EFI\Microsoft\Boot\refind_x64.efi S:\EFI\Microsoft\Boot\refind_x64.bak
+ren S:\EFI\Microsoft\Boot\refind_x64.efi bootmgfw.efi
+mountvol S: /D
+pause
